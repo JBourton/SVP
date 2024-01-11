fcd1ccb7c1aea08273d612513b049c818b2c462a
JBourton <joshua.bourton16@gmail.com>
2024-01-11 15:37:41 +0000

    Add command line argument reader

--- main.c
+++ main.c
@@ -1,10 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
 #include "svp_structs.h"
 #include "lll.h"
 
 // Main should be able to receive an arbitary number of input vectors
 int main(int argc, char *argv[]) {
+    // Take vector inputs for the basis from the command line
+    if (argc < 2) {
+        printf("Input the basis in the form: %s [v1] [v2] ... [vn]\n", argv[0]);
+        return 0;
+    }
+
+    int open_brackets = 0;
+    int closing_brackets = 0;
+
+    // Make 1 big string from all the command line arguments
+    size_t input_length = 0;
+
+    // Check number of opening and closing brackets match
+    for (int i=1; i<argc; i++) {
+        if (strncmp(argv[i],"[", 1) == 0) {
+            open_brackets += 1;
+        }
+        size_t input_item = strlen(argv[i]);
+        input_length += input_item + 1;
+        if (input_item > 0 && argv[i][input_item- 1] == ']') {
+            closing_brackets += 1;
+        }
+    }
+    if (open_brackets != closing_brackets) {
+        printf("[INPUT ERROR] Number of openinig and closing brackets do not match\n");
+        return 0;
+    }
+
+    // Assign dynamic memory to the string containing all the inputs
+    char *mega_input = (char *)malloc(input_length);
+    if (mega_input == NULL) {
+        printf("[MEMORY ALLOCATION ERROR]\n");
+        return 0;
+    }
+
+    // Put the inividaul commandl ine arguments into a single string
+    for (int i = 1; i < argc; ++i) {
+        strcat(mega_input, argv[i]);
+        if (i < argc - 1) {
+            strcat(mega_input, " ");
+        }
+    }
+    printf("Concatenated String: %s\n", mega_input);
+
+
+
+    free(mega_input);
+
+    // Also need to validate instances where brackets could match count but be wrong e.g. []5[[]-]
+
+
     int numVectors;
     int dimension;
     printf("Enter the number of vectors: ");
